a03ec6fa4362a3fdc3fddb1027733d77e2b48156
YoMed18 <yomed18@gmail.com>
2024-01-11 14:24:42 +0100

    add scroll

--- src/pages/Bundle.tsx
+++ src/pages/Bundle.tsx
@@ -10,12 +10,12 @@ import {
   CardTitle,
 } from "../components/ui/card";
 import { Button } from "../components/ui/button";
-
+import { ScrollArea } from "@/components/ui/scroll-area"
 import {
     Dialog,
     DialogContent,
     DialogDescription,
-    DialogFooter,
+    //DialogFooter,
     DialogHeader,
     DialogTitle,
     DialogTrigger,
@@ -57,17 +57,15 @@ const Bundle: React.FC = () => {
                     <img src="https://cdn.helloasso.com/img/logos/yeswiki-3a2178a821694d8297d746434ab6f356.png?resize=fill:140:140" alt="" className='inline-block w-[20%] h-auto rounded-[50%] border-[none] block m-auto'/>
                     
                     <DialogDescription>
-                        <Card className='mt-[5%] mb-[-15%]'>
-                            <CardContent className='p-[2%]'>
-                                <p>This action cannot be undone. This will permanently delete your account
-                                    and remove your data from our servers.</p>
-                            </CardContent>
-                        </Card>
+
+                    <ScrollArea className="h-[95%] rounded-md border p-4">
+                    <p className='pb-[5%]'>This action cannot be undone. This will permanently delete your account
+                      and remove your data from our servers.</p>
+                      <img src="https://interpole.xyz/cache/YeswikI2_imagebf_image_Screenshot_20221108_at_142514_Bienvenue_sur_YesWiki_vignette_600_600_20221108142653_20221108142653.png" alt="" className='w-auto h-[90%]'/>
+
+                    </ScrollArea>
                     </DialogDescription>
                   </DialogHeader>
-                  <DialogFooter>
-                    <img src="https://interpole.xyz/cache/YeswikI2_imagebf_image_Screenshot_20221108_at_142514_Bienvenue_sur_YesWiki_vignette_600_600_20221108142653_20221108142653.png" alt="" className='w-auto h-[90%]'/>
-                  </DialogFooter>
                 </DialogContent>
               </Dialog>
             
