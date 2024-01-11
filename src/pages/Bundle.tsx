48e292af3c4bfe14e3dc8dcb79e719c2cadc2cc5
YoMed18 <yomed18@gmail.com>
2024-01-11 15:09:36 +0100

    update

--- src/pages/Bundle.tsx
+++ src/pages/Bundle.tsx
@@ -15,13 +15,10 @@ import {
     Dialog,
     DialogContent,
     DialogDescription,
-    //DialogFooter,
     DialogHeader,
     DialogTitle,
     DialogTrigger,
   } from "@/components/ui/dialog"
-  //import { Input } from "@/components/ui/input"
-  //import { Label } from "@/components/ui/label"
 
 
 const Bundle: React.FC = () => {
