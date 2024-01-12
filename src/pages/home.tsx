12516a15d89558d2ac1bc1f72bd5784a43938814
lis-a <100594087+lis-a@users.noreply.github.com>
2024-01-12 11:09:18 +0100

    Update home.tsx

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,11 +1,19 @@
 import BundleList from '@/components/bundle-list';
 import { buttonVariants } from '@/components/ui/button';
 import { cn } from '@/lib/utils';
+import { useNavigate } from 'react-router-dom';
 
 export default function Home() {
+
+    const navigate = useNavigate();
+
+    const goCreateBundlePage = () => {
+        navigate(`/bundle/create`);
+    };
+
     return (
         <div className="flex flex-col gap-4">
-            <a className={cn(buttonVariants({ variant: 'default' }), 'w-fit')}>Créer un bundle</a>
+            <a className={cn(buttonVariants({ variant: 'default' }), 'w-fit cursor-pointer')} onClick={() => goCreateBundlePage()}>Créer un bundle</a>
             <BundleList />
         </div>
     );
